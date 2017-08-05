45d5309c988c6f8d1288cc09ff86cce0fe7061ea
jamesrford7 <jamesrford7@gmail.com>
2017-08-06 00:50:02 +0100

    Iteratively add the TimeAndTempCells.

--- client/components/WeatherRow.jsx
+++ client/components/WeatherRow.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import DateCell from './DateCell.jsx';
+import TimeAndTempCell from './TimeAndTempCell.jsx';
 
 export default function WeatherRow(props){
+  const timeAndTemps = props.timeAndTemps.map((timeAndTemp) => {
+    return <TimeAndTempCell key={timeAndTemp.dt} time={timeAndTemp.time} temp={timeAndTemp.temp} />
+  });
+
   return(
-    <DateCell day={props.day} month={props.month} date={props.date} />
+    <div>
+      <DateCell day={props.day} month={props.month} date={props.date} />
+      {timeAndTemps}
+    </div>
   );
 }
--- client/components/WeatherRow.test.js
+++ client/components/WeatherRow.test.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import WeatherRow from './WeatherRow';
 
-const sut = shallow(<WeatherRow day='Mon' month='Sept' date='3' />);
-
 describe('WeatherRow', () => {
+  let sut;
+
+  const timeAndTemps = [
+    {dt: 1234, time: '9AM', temp: '15'},
+    {dt: 3213, time: '12AM', temp: '12'},
+    {dt: 3634, time: '3PM', temp: '22'}
+  ];
+
+  beforeAll(() => {
+    sut = shallow(<WeatherRow day='Mon' month='Sept' date='3' timeAndTemps={timeAndTemps} />);
+  });
+
   it('should contain a DateCell', () => {
     expect(sut.find('DateCell').length).toEqual(1);
   });
@@ -14,5 +24,19 @@ describe('WeatherRow', () => {
     expect(dataCellProps.day).toEqual('Mon');
     expect(dataCellProps.month).toEqual('Sept');
     expect(dataCellProps.date).toEqual('3');
+  });
+
+  it('should create the correct amount of TimeAndTempCells', () => {
+    expect(sut.find('TimeAndTempCell').length).toEqual(3);
+  });
+
+  it('should correctly populate the TimeAndTempCells', () => {
+    const timeAndTempNodes = sut.find('TimeAndTempCell').nodes;
+    expect(timeAndTempNodes[0].props.time).toEqual('9AM');
+    expect(timeAndTempNodes[0].props.temp).toEqual('15');
+    expect(timeAndTempNodes[1].props.time).toEqual('12AM');
+    expect(timeAndTempNodes[1].props.temp).toEqual('12');
+    expect(timeAndTempNodes[2].props.time).toEqual('3PM');
+    expect(timeAndTempNodes[2].props.temp).toEqual('22');
   });
 });
