eafbc86773a5c90605e13407a5a1b5c40e5ca698
jamesrford7 <jamesrford7@gmail.com>
2017-08-06 18:31:06 +0100

    Build up the list of time and temp objects for each day

--- client/utilities/dto.js
+++ client/utilities/dto.js
@@ -1,8 +1,20 @@
 export function convertToDto(data) {
   let dto = {};
-  dto.weatherRows = [];
+  dto.weatherRows = generateDates(data);
 
+  dto.weatherRows.forEach(weatherRow => {
+    weatherRow.timeAndTemps = generateDateTimes(data, weatherRow);
+  });
+
+  dto.cityName = data.city.name;
+  console.log(dto);
+  return dto;
+}
+
+function generateDates(data) {
+  let result = [];
   let uniqueDates = [];
+
   data.list.forEach(item => {
     let date = item.dt_txt.split(' ')[0];
     if (uniqueDates.indexOf(date) === -1) {
@@ -10,12 +22,33 @@ export function convertToDto(data) {
     }
   });
 
-  uniqueDates.forEach(date => {
-    let splitDate = date.split('-');
+  uniqueDates.forEach(datestamp => {
+    let splitDate = datestamp.split('-');
     let dateObject = new Date(splitDate[0], splitDate[1] - 1, splitDate[2]);
-    dto.weatherRows.push({date: dateObject.toDateString()});
+    result.push({date: dateObject.toDateString(), datestamp: datestamp});
   });
 
-  dto.cityName = data.city.name;
-  return dto;
+  return result;
+}
+
+function generateDateTimes(data, weatherRow) {
+  let result = [];
+
+  let currentDate = weatherRow.datestamp;
+  data.list.forEach(item => {
+    if(item.dt_txt.split(' ')[0] == currentDate) {
+      let dt = item.dt;
+      let time = item.dt_txt.split(' ')[1];
+      let temp = item.main.temp;
+      let icon = item.weather[0].icon;
+      result.push({
+        dt: dt,
+        time: time,
+        temp: temp,
+        icon: icon
+      });
+    }
+  });
+
+  return result;
 }
--- client/utilities/dto.test.js
+++ client/utilities/dto.test.js
@@ -18,6 +18,22 @@ describe('DTO', () => {
   });
 
   it('should convert the dates to date strings', () => {
-    expect(dateList[0]).toEqual('Sun Aug 06 2017');
+    const expectedDateStrings = [
+      'Sun Aug 06 2017',
+      'Mon Aug 07 2017',
+      'Tue Aug 08 2017',
+      'Wed Aug 09 2017',
+      'Thu Aug 10 2017'
+    ]
+
+    for(var i = 0; i < expectedDateStrings.length; i++) {
+      expect(dateList[i]).toEqual(expectedDateStrings[i]);
+    }
   });
+
+  it('should return the correct amount of timeAndTemp objects', () => {
+    result.weatherRows.forEach((weatherRow) => {
+      expect (weatherRow.timeAndTemps.length).toEqual(8);
+    });
+  })
 });
